040be7cfba06d5d9e245c1c4b6d25144a5749a2f
Team DeadlineTech <tosu80018@gmail.com>
2025-06-19 02:44:01 +0530

    Update start.py

--- DeadlineTech/utils/inline/start.py
+++ DeadlineTech/utils/inline/start.py
@@ -30,16 +30,17 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_10"], user_id=config.OWNER_ID), 
+             
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
         ],
 
         [
-            InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL)
+            InlineKeyboardButton(text=_["S_B_10"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT)
+            
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], web_app=WebAppInfo(url="https://vps-kappa.vercel.app")), 
+            InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL), 
             InlineKeyboardButton(text=_["S_B_5"], url="https://github.com/DeadlineTech/music")
         ]
     ]
