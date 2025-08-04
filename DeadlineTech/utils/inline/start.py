a56598164ad198e95427a5b602eb45e3fc5d5775
GhosttBatt <ghosttbatt@gmail.com>
2025-08-04 17:22:26 +0530

    start.py

--- DeadlineTech/utils/inline/start.py
+++ DeadlineTech/utils/inline/start.py
@@ -11,12 +11,6 @@ def start_panel(_):
             ), 
            # InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
         ],
-        ''' [
-            InlineKeyboardButton(
-                text=_["S_B_5"],
-                url="https://github.com/DeadlineTech/music"
-            )
-        ] '''
     ]
     return buttons
 
@@ -33,15 +27,5 @@ def private_panel(_):
              
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
         ],
-
-       ''' [
-            InlineKeyboardButton(text=_["S_B_10"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT)
-            
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL), 
-            InlineKeyboardButton(text=_["S_B_5"], url="https://github.com/DeadlineTech/music")
-        ] '''
     ]
     return buttons
