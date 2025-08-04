c121fb35962eab8d8ea387bb5c6cfeee54e96422
GhosttBatt <ghosttbatt@gmail.com>
2025-08-04 17:22:57 +0530

    start.py

--- DeadlineTech/utils/inline/start.py
+++ DeadlineTech/utils/inline/start.py
@@ -9,7 +9,6 @@ def start_panel(_):
                 text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true"
             ), 
-           # InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
         ],
     ]
     return buttons
