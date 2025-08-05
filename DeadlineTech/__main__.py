2a625091ebda0bcb03e397e7de11ed4fa4c3aef7
rajs2695 <rajeshrakis2695@gmail.com>
2025-08-05 09:04:05 +0530

    __main__.py

--- DeadlineTech/__main__.py
+++ DeadlineTech/__main__.py
@@ -99,6 +99,7 @@ async def init():
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(init())
     keep_alive()
     set_webhook()
+    asyncio.get_event_loop().run_until_complete(init())
+    
