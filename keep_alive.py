0e326832741225c8ed01053761f5abaad1a21543
rajs2695 <rajeshrakis2695@gmail.com>
2025-08-05 08:43:59 +0530

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -10,7 +10,7 @@ def home():
     return "Bot is running on Render!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=10000)
 
 def keep_alive():
     t = Thread(target=run)
