18b1d77cc7564d1ba3773ba30d94eabfff83621d
rajs2695 <rajeshrakis2695@gmail.com>
2025-08-05 06:55:06 +0530

    Youtube.py

--- DeadlineTech/platforms/Youtube.py
+++ DeadlineTech/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from SHUKLAMUSIC.utils.database import is_on_off
-from SHUKLAMUSIC.utils.formatters import time_to_seconds
+from DeadlineTech.utils.database import is_on_off
+from DeadlineTech.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
@@ -26,7 +26,7 @@ async def shell_cmd(cmd):
     return out.decode("utf-8")
 
 
-cookies_file = "SHUKLAMUSIC/assets/cookies.txt"
+cookies_file = "cookies/HeartBeat.txt"
 
 class YouTubeAPI:
     def __init__(self):
